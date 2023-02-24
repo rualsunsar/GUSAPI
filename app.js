1095fbfe48608720099bc87028af5056ef7309c8
rualsunsar <rualsunsar@163.com>
2023-02-24 16:34:24 +0800

    初始化

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const expressJwt = require('express-jwt')
+const bodyParser = require('body-parser')
+const userRouter = require('./router/user')
+const roleRouter = require('./router/role')
+const menuRouter = require('./router/menu')
+const logRouter = require('./router/user-log')
+
+const history = require('connect-history-api-fallback')
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use(history())
+
+app.use(expressJwt({
+  secret: 'bigfool.cn' // 签名的密钥 或 PublicKey
+}).unless({
+  path: ['/', '/user/login'] // 指定路径不经过 Token 解析
+}))
+
+app.use(bodyParser.urlencoded({
+  extends: false
+}))
+
+app.use(bodyParser.json())
+
+app.use(function(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('token已失效')
+  } else {
+    res.status(500).send('服务器错误')
+  }
+})
+
+//  使用路由 / 是路由指向名称
+app.use('/user', userRouter)
+app.use('/user-log', logRouter)
+app.use('/role', roleRouter)
+app.use('/menu', menuRouter)
+
+// 配置服务端口
+const port = 8002
+const hostname = '0.0.0.0'
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+
---
+++ model/init.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(
+  'sar_test', // 数据库名
+  'root', // 用户名
+  'Sar621214#', // 密码
+  {
+    'dialect': 'mysql', // 数据库类型
+    'host': '124.222.91.249', // ip
+    'port': 3306, // 端口
+    define: {
+      timestamps: false
+    },
+    timezone: '+08:00' // 东八时区
+  }
+)
+
+module.exports = sequelize
