97861b3b3d912fa7faf9f4da1c36a45b0ca8b667
rualsunsar <rualsunsar@163.com>
2023-03-06 10:46:49 +0800

    Update webApi.js

--- router/webApi.js
+++ router/webApi.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const ArticleModel = require('../model/articles')
 
+/**
+ * 获取文章列表
+ */
 router.get('/articleList', (req, res, next) => {
   console.log('/articleList')
     ArticleModel.findAll({
@@ -17,6 +20,23 @@ router.get('/articleList', (req, res, next) => {
         data: data
       })
     })
+})
+
+/**
+ * 获取文章详情
+ */
+router.get('/articleInfo', (req, res, next) => {
+  ArticleModel.findOne({
+    where: {
+      article_id: req.query.article_id || 0
+  }
+  }).then(function(article) {
+    return res.json({
+      code: 20000,
+      message: '获取成功',
+      data: article
+    })
+  })
 })
 
 module.exports = router
